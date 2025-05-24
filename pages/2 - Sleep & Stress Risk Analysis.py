bd75c66a6f499e3924fe286e0be1f14a0cd7d242
HienVoPhuong <106240134@student.vgu.edu.vn>
2025-05-25 01:16:19 +0700

    Update 2 - Sleep & Stress Risk Analysis.py

--- pages/2 - Sleep & Stress Risk Analysis.py
+++ pages/2 - Sleep & Stress Risk Analysis.py
@@ -6,7 +6,7 @@ import joypy
 import time
 
 # -------------------- PAGE CONFIG --------------------
-st.set_page_config(page_title="🧠Sleep & Stress Risk Analysis", layout="wide")
+st.set_page_config(page_title="🩺 Sleep & Stress Risk Analysis", layout="wide")
 
 # --------- GLOBAL FONT STYLING ---------
 st.markdown("""
