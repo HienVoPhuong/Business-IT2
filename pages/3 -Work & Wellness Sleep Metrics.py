b4effcee6e4b2e0122534ffa10d60885fd3f8e4f
HienVoPhuong <106240134@student.vgu.edu.vn>
2025-05-25 01:15:59 +0700

    Update 3 -Work & Wellness Sleep Metrics.py

--- pages/3 -Work & Wellness Sleep Metrics.py
+++ pages/3 -Work & Wellness Sleep Metrics.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # --------- Page Configuration ---------
-st.set_page_config(page_title="Occupational Sleep Trends & Cardiovascular Metrics", layout="wide")
+st.set_page_config(page_title="💗 Occupational Sleep Trends & Cardiovascular Metrics", layout="wide")
 
 # --------- Font Styling ---------
 st.markdown("""
