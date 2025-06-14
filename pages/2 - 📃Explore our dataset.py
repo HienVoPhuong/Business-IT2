16d105714a1bdea8bd0e76f833d530973632b0ad
HienVoPhuong <106240134@student.vgu.edu.vn>
2025-06-14 15:39:04 +0700

    Update 2 - 📃Explore our dataset.py

--- pages/2 - 📃Explore our dataset.py
+++ pages/2 - 📃Explore our dataset.py
@@ -273,7 +273,7 @@ if selected_age_range:
     filtered_df = filtered_df[(filtered_df["Age"] >= selected_age_range[0]) & (filtered_df["Age"] <= selected_age_range[1])]
 
 st.markdown(
-    f"Showing <b>{len(filtered_df):,}</b> of <b style='color:red;'>{len(df):,}</b> records",
+    f"Showing <b style='color:blue;'{len(filtered_df):,}</b> of <b style='color:red;'>{len(df):,}</b> records",
     unsafe_allow_html=True
 )
 st.dataframe(filtered_df.reset_index(drop=True), use_container_width=True)
